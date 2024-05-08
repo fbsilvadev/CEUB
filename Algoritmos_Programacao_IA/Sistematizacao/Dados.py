02727393e43050e98d67d231b7c9f3526f96f751
Fernando Silva <fernando.silva@confea.org.br>
2024-05-08 17:27:22 -0300

    Atualizando dados

---
+++ Algoritmos_Programacao_IA/Sistematizacao/Dados.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+
+# Amostras
+num_amostras = 1000
+
+# Gerar dados aleat�rios
+idades = np.random.randint(18, 70, size=num_amostras)
+rendas_mensais = np.random.uniform(1000, 10000, size=num_amostras)
+historicos_credito = np.random.choice(['Bom', 'Ruim'], size=num_amostras)
+historicos_emprego = np.random.choice(['Estavel', 'Instavel'], size=num_amostras)
+aprovados = np.random.choice([0, 1], size=num_amostras)
+
+# Criar DataFrame
+dados = pd.DataFrame({
+    'Idade': idades,
+    'Renda Mensal': rendas_mensais,
+    'Historico de Credito': historicos_credito,
+    'Historico de Emprego': historicos_emprego,
+    'Aprovado': aprovados
+})
+
+# Salvar os dados em um arquivo CSV
+dados.to_csv('dados_de_treinamento.csv', index=False)
